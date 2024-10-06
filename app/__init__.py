35b5c5b0c9621069ef3c8f43692205f51a4558aa
Andrey Kulenko <ankubel904@gmail.com>
2024-10-06 04:22:54 +0300

    Добавить конфигурацию базы данных

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.blueprints.auth.routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('app.config')
     
     # Регистрация блюпринта auth
     app.register_blueprint(auth_bp, url_prefix='/')
