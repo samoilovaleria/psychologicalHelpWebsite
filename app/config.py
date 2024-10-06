8003c226faad5a1cb2d9fc759284c8a003e25977
Andrey Kulenko <ankubel904@gmail.com>
2024-10-06 05:31:39 +0300

    Исправить конфигурацию базы данных
    Я перечислял параметры через запятую, что делало вместо нужных
    значений кортежи - убрал запятые. Также избавился от лишнего
    параметра `DATABASE`, который можно указывать напрямую с
    помощью `app.config["DATABASE"]`

--- app/config.py
+++ app/config.py
@@ -1,13 +1,10 @@
 class DatabaseConfig:
-    HOST: str = "localhost",
-    PORT: int = 5432,
-    USER: str = "admin",
-    PASS: str = "pass",
+    HOST: str = "localhost"
+    PORT: int = 5432
+    USER: str = "admin"
+    PASS: str = "pass"
     NAME: str = "polytech"
 
     @property
     def URI(self) -> str:
         return f"postgresql://{self.USER}:{self.PASS}@{self.HOST}:{self.PORT}/{self.NAME}"
-
-
-DATABASE = DatabaseConfig()
