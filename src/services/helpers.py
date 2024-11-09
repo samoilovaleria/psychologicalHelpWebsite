433e105c998700d1c8f5c975e47e45f17d66b038
glebanazarenko <glebanazarenko@gmail.com>
2024-11-09 12:08:30 +0300

    Добавил hash_password

--- src/services/helpers.py
+++ src/services/helpers.py
@@ -12,3 +12,6 @@ def create_access_token(sub: dict[str, str]) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
