2bb622f705a12bac579eb640c0f9c973aca8e986
glebanazarenko <glebanazarenko@gmail.com>
2024-11-09 19:29:15 +0300

    Удалил лишние функции

--- src/routes/controllers/users_routes.py
+++ src/routes/controllers/users_routes.py
@@ -14,13 +14,6 @@ from sqlalchemy.exc import IntegrityError
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-# @router.get("/{user_id}", response_model=UserBase)
-# def read_user(user_id: int):
-#     user = get_user_by_id(user_id)
-#     if user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return user
-
 @router.get("/{user_id}", response_model=UserBase)
 async def read_user(user_id: UUID):
     user = await get_user_by_id(user_id)
