c6017aab0d65fc7c2c564058c888ee34c4fa08a2
Andrey Kulenko <ankubel904@gmail.com>
2024-11-12 19:57:33 +0300

    Добавить ограничение на первичный ключ

--- src/models/roles_model.py
+++ src/models/roles_model.py
@@ -1,20 +1,22 @@
 import uuid
-from sqlalchemy import Column, ForeignKey, Enum
+import enum
+
+from sqlalchemy import Column, ForeignKey, Enum, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
 from src.config.database import Base
-import enum
 from sqlalchemy.dialects.postgresql import UUID
 
 class UserRole(enum.Enum):
     Student = 'Student'
     Therapist = 'Therapist'
     Administrator = 'Administrator'
-    Stuff = 'University employee'
+    Stuff = 'Stuff'
 
 class Role(Base):
     __tablename__ = 'roles'
     
     user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete='CASCADE'))
-    role = Column(Enum(UserRole), primary_key=True, nullable=False)
+    role = Column(Enum(UserRole), nullable=False)
 
+    __table_args__ = (PrimaryKeyConstraint("user_id", "role"))
     user = relationship("User", back_populates="roles")
