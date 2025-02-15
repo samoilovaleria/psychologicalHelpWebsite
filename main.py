d6c21f11a59f682cbe7dd802c74b33f53b593cc0
Andrey Kulenko <ankubel904@gmail.com>
2025-02-15 17:46:14 +0300

    Убрать комментарии

--- main.py
+++ main.py
@@ -8,71 +8,46 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.declarative import declarative_base
 from src.config.database import Base, config, RESET_DB_ON_START, RESET_COOKIE_ON_START
 
-# # Определяем базу для моделей
-# Base = declarative_base()
 
-# Функция для сброса и пересоздания таблиц
 async def reset_database(engine: AsyncEngine):
     async with engine.begin() as conn:
-        # Удаление всех таблиц
-        # print("Удаление всех таблиц...")
         await conn.run_sync(Base.metadata.drop_all)
-        
-        # Создание новых таблиц
-        # print("Создание таблиц...")
         await conn.run_sync(Base.metadata.create_all)
-
-    # print("Таблицы успешно пересозданы.")
 
 
 async def clear_all_cookies():
-    # Здесь устанавливаем пустые cookie с истекшим временем
     response = Response()
-    cookies_to_clear = ["access_token"]  # Добавьте имена cookie, которые хотите очистить
+    cookies_to_clear = ["access_token"]
 
     for cookie in cookies_to_clear:
         response.delete_cookie(
             key=cookie,
             httponly=True,
-            secure=False,  # Установите в False для локального тестирования
-            samesite="Lax",  # Используем "Lax" для лучшей совместимости
+            secure=False,
+            samesite="Lax",
         )
 
-    # print("Все cookie успешно очищены при запуске приложения.")
 
-
-# Создаем асинхронный движок подключения
 engine = create_async_engine(config.DATABASE_URL, echo=True)
 
 
 def get_application() -> FastAPI:
-    # TODO:
-    # application = FastAPI(
-    #     title=settings.PROJECT_NAME,
-    #     debug=settings.DEBUG,
-    #     version=settings.VERSION
-    # )
     application = FastAPI()
     application.include_router(api_router)
 
     application.add_middleware(
         CORSMiddleware,
-        # TODO:
-        # allow_origins=settings.CORS_ALLOWED_ORIGINS.split(" "),
-        allow_origins=["*"],  # Разрешить все домены
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
 
-    # Добавляем обработчик события startup
     @application.on_event("startup")
     async def on_startup():
-        # Вызов функции сброса и создания таблиц
         if RESET_DB_ON_START:
             await reset_database(engine)
 
-        # Очищаем все cookie
         if RESET_COOKIE_ON_START:
             await clear_all_cookies()
 
