7436aedcc8f5112ac638b0b03e1f52eaca2ed7de
Andrey Kulenko <ankubel904@gmail.com>
2025-02-24 18:54:59 +0300

    Исправить выдачу психолога

--- src/services/therapists_service.py
+++ src/services/therapists_service.py
@@ -3,8 +3,27 @@ from uuid import UUID
 
 from fastapi import Request
 
-def get_therapist_by_id(therapist_id: UUID):
-    return get_therapist(therapist_id)
+async def get_therapist_by_id(therapist_id: UUID):
+    therapist = get_therapist(therapist_id)
+
+    if therapist is None:
+        return None
+
+    return {
+        "id": str(therapist.id),
+        "experience": therapist.experience,
+        "qualification": therapist.qualification,
+        "consult_areas": therapist.consult_areas,
+        "description": therapist.description,
+        "office": therapist.office,
+        "first_name": therapist.user.first_name,
+        "middle_name": therapist.user.middle_name,
+        "last_name": therapist.user.last_name,
+        "phone_number": therapist.user.phone_number,
+        "email": therapist.user.email,
+        "education": therapist.education,
+        "short_description": therapist.short_description,
+    }
 
 async def get_all_therapists(request: Request, skip: int = 0, take: int = 10):
     """
