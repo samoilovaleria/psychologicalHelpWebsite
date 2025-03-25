f382363acefa328748dc4869f79a8a9b46232d35
Andrey Kulenko <ankubel904@gmail.com>
2025-03-25 05:38:16 +0300

    Удалить отзывы из контроллеров

--- psychohelp/routes/routes.py
+++ psychohelp/routes/routes.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from .controllers import users
 from .controllers import appointments
-from .controllers import reviews
 from .controllers import therapists
 from .controllers import images
 
@@ -9,6 +8,5 @@ from .controllers import images
 api_router = APIRouter()
 api_router.include_router(users.router)
 api_router.include_router(appointments.router)
-api_router.include_router(reviews.router)
 api_router.include_router(therapists.router)
 api_router.include_router(images.router)
