2e01de232c6d8743aee077cd4ce33cc318de40eb
Andrey Kulenko <ankubel904@gmail.com>
2025-03-25 06:31:22 +0300

    Структурировать сервис пользователей

--- psychohelp/services/users.py
+++ psychohelp/services/users.py
@@ -1,79 +1,42 @@
-from psychohelp.repositories.users import get_user, create_user
-from psychohelp.repositories import (
-    create_access_token,
-    verify_password,
-    get_user_email_from_token,
-    get_token_from_cookie,
-    set_token_in_cookie,
-)
+from psychohelp.repositories import create_access_token, verify_password
 from psychohelp.repositories.users import (
-    get_user,
+    get_user_by_id as repo_get_user_by_id,
     get_user_by_email as repo_get_user_by_email,
+    get_user_by_token as repo_get_user_by_token,
+    create_user,
 )
-
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.exc import IntegrityError
-
-from fastapi import Request, Response, HTTPException
-
-
-async def register_user(user_data, response: Response):
-    new_user = await create_user(user_data)
-    token = create_access_token({"sub": new_user.email})
-    set_token_in_cookie(response, token)
-    return {"user": new_user, "access_token": token, "token_type": "bearer"}
 
 
 async def get_user_by_id(user_id: UUID):
-    user = await get_user(user_id)
-
-    if user is None:
-        return None
-
-    return {
-        "id": user.id,
-        "first_name": user.first_name,
-        "middle_name": user.middle_name,
-        "last_name": user.last_name,
-        "phone_number": user.phone_number,
-        "email": user.email,
-        "social_media": user.social_media,
-    }
+    return await repo_get_user_by_id(user_id)
 
 
 async def get_user_by_email(email: str):
-    user = await repo_get_user_by_email(email)
-
-    if user is None:
-        return None
-
-    return {
-        "id": user.id,
-        "first_name": user.first_name,
-        "middle_name": user.middle_name,
-        "last_name": user.last_name,
-        "phone_number": user.phone_number,
-        "email": user.email,
-        "social_media": user.social_media,
-    }
-
-
-async def user_login(email: str, password: str, response: Response):
-    user = await repo_get_user_by_email(email)
-    if not user or not verify_password(password, user.password):
-        return None
-
-    token = create_access_token({"sub": user.email})
-    set_token_in_cookie(response, token)
-
-    return {"access_token": token, "token_type": "bearer"}
+    return await repo_get_user_by_email(email)
 
 
 async def get_user_by_token(token: str):
-    email = await get_user_email_from_token(token)
+    return repo_get_user_by_token(token)
+
+
+async def register_user(
+    first_name: str,
+    last_name: str,
+    phone_number: str,
+    email: str,
+    password: str,
+    middle_name: str | None = None,
+    social_media: str | None = None,
+):
+    new_user = await create_user(
+        first_name, last_name, phone_number, email, password, middle_name, social_media
+    )
+    return create_access_token(new_user.id)
+
+
+async def login_user(email: str, password: str):
     user = await repo_get_user_by_email(email)
-    return user
+    if user is None or not verify_password(password, user.password):
+        return None
 
-
-async def get_token(request: Request):
-    return await get_token_from_cookie(request)
+    return create_access_token(user.id)
