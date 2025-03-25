954102a43b1b5758a1c8e92ce11d6627154b6cba
Andrey Kulenko <ankubel904@gmail.com>
2025-03-25 07:43:07 +0300

    Реализовать получение ролей

--- psychohelp/repositories/roles.py
+++ psychohelp/repositories/roles.py
@@ -8,4 +8,4 @@ from sqlalchemy.dialects.postgresql import UUID
 async def get_roles_by_user_id(user_id: UUID):
     async with get_async_db() as session:
         result = await session.execute(select(Role).filter(Role.user_id == user_id))
-    return result.all()
+    return result.scalars().all()
--- psychohelp/routes/controllers/roles.py
+++ psychohelp/routes/controllers/roles.py
@@ -1,16 +1,18 @@
 from fastapi import HTTPException, APIRouter
 
+from starlette.status import HTTP_404_NOT_FOUND
+
 from psychohelp.services.roles import get_roles_by_id
-from psychohelp.schemas.roles import RoleBase
+from psychohelp.models.roles import UserRole
 
 from uuid import UUID
 
 router = APIRouter(prefix="/roles", tags=["roles"])
 
 
-@router.get("/{user_id}", response_model=list[RoleBase])
+@router.get("/{user_id}", response_model=list[UserRole])
 async def get_roles(user_id: UUID):
-    roles = await get_roles_by_id(user_id)
-    if roles is None:
-        raise HTTPException(status_code=404, detail="Role not found")
+    roles = [r.role for r in await get_roles_by_id(user_id)]
+    if not roles:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Роли не найдены")
     return roles
